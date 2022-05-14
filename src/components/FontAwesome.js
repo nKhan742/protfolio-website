fe298bdb99a7e796942b113cd5fe9612978a6307
nKhan742 <69765547+nKhan742@users.noreply.github.com>
2022-05-14 16:37:43 +0530

    First comit

---
+++ src/components/FontAwesome.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faCoffee, faPhone } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckSquare, faCoffee, faPhone)
---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Header = () => {
+    return(
+        <nav className="navbar navbar-expand-lg">
+            <div className="container">
+                <a className="navbar-brand" href="#">Khan</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="" id="navbarSupportedContent">
+                    <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                        <a className="nav-link active px-4" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link px-4" href="#">About Us</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link px-4" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link px-4" href="#">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link px-4" href="#">Contact Me</a>
+                        </li>
+                        <li className="nav-item d-flex align-items-center">
+                        |
+                        </li>
+                        <li className="nav-item  px-4 d-flex align-items-center">
+                            <FontAwesomeIcon icon="phone" size="sm"/>&nbsp;
+                            <p className="m-0"><a className=" primary2">99-71-80-1873</a></p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </nav>
+    )
+}
+
+export default Header;
